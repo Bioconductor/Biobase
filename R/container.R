0f29ddf633d9fc6a657f6e578401fcdea0db7d11
Robert Gentleman <rgentlem@fhcrc.org>
2001-10-10 16:00:32 +0000

    started on a simple container class
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@105 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/container.R
@@ -0,0 +1,47 @@
+#Copyright 2001, R. Gentleman, all rights reserved.
+
+#A simple class structure for containers
+#These are simply lists where the list can contain only
+#objects of the specified class
+
+
+setClass("container", representation( x = "list", content =
+                                     "character", locked = "logical"),
+         prototype = list(x=vector("list", 0), content="object",
+                                     locked=FALSE))
+
+if( !isGeneric("content") )
+    setGeneric("content", function(object)
+ standardGeneric("content"))
+
+setMethod("content", "container", function(object) object@content)
+
+if( !isGeneric("locked") )
+    setGeneric("locked", function(object)
+ standardGeneric("locked"))
+
+setMethod("locked", "container", function(object) object@locked)
+
+
+
+setReplaceMethod("[[", "container", function(x, i, j, ..., value) {
+    if( locked(x) )
+        stop("cannot assign into a locked container")
+    cv <- class(value)
+    cont <- content(x)
+    if( !extends(cv, cont) )
+        stop(paste("the container is class", cont,
+                   "the object is class", cv, "cannot assign",
+                   sep=" "))
+    object@x[[i]] <- value
+})
+
+setMethod("[[", "container", function(x, i, j,...) {
+    x@x[[i]]
+})
+
+setMethod("print", "container", function(x, ...) {
+    cat("Container of ", content(x), "\n", sep="")
+    print(x@x)
+})
+
