bf8eaded8eb925453bb9c241a5e27f6576d56847
Robert Gentleman <rgentlem@fhcrc.org>
2001-11-12 22:26:11 +0000

    a class for expression arrays
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@179 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/exprSet.R
@@ -0,0 +1,74 @@
+# A class of for microarray data
+
+# in this representation we think of the data set being comprised of
+# an expression array, uexpr, the sample names, the gene names
+# for plotting we have dendrogram on each axis,
+# phenotypic data -- associated with sample labels
+
+require(methods)
+
+.initClasses <- function(where) {
+ setClass("exprSet", representation(exprs="matrix",
+                                   phenodata="data.frame",
+                                    description="character") )
+
+#define a generic for obtaining the data
+ if( !isGeneric("exprs") )
+     setGeneric("exprs", function(object) standardGeneric("exprs"))
+ setMethod("exprs", "exprSet", function(object) object@exprs)
+
+ if( !isGeneric("phenodata") )
+     setGeneric("phenodata", function(object)
+                standardGeneric("phenodata"))
+ setMethod("phenodata", "exprSet", function(object)
+         object@phenodata )
+
+ if( !isGeneric("sampleNames") )
+     setGeneric("sampleNames", function(object)
+                standardGeneric("sampleNames"))
+ setMethod("sampleNames", "exprSet",
+           function(object) names(phenodata))
+
+ if( !isGeneric("geneNames") )
+     setGeneric("geneNames", function(object)
+                standardGeneric("geneNames"))
+ setMethod("geneNames", "exprSet", function(object) row.names(object@exprs) )
+
+
+ if( !isGeneric("[") )
+     setGeneric("[")
+
+ setMethod("[", "exprSet", function(x, i, j, ..., drop=TRUE)
+     new("exprSet", exprs=x@exprs[i,j], phenodata = x@phenodata[j,,drop=FALSE],
+     description=x@description))
+
+
+# if( !isGeneric("plot") )
+#    setGeneric("plot")
+
+# setMethod("plot", "uarray", function(object, ...) {
+#     expr <- as.matrix(uexpr(object))
+#     #scale
+#     expr <- sweep(expr, 1, apply(expr, 1, mean, na.rm = TRUE))
+#     f <- function(v) {
+#         v <- v[!is.na(v)]
+#         sqrt(sum(v^2)/max(1, length(v) - 1))
+#     }
+#     expr <- sweep(expr, 1, apply(expr, 1, f), "/")
+#     breaks <- seq(-3,3,by=.2)
+#     colors<- GetColor(breaks)
+#     breaks <- c(-100,breaks,100)
+#     colors <- c(colors[1], colors)
+#     opar<-par(mar=c(1,1,4,10))
+#     on.exit(par(mar=opar))
+#     image(1:ncol(expr), 1:nrow(expr), z = t(expr), axes = F,
+#     col=colors, breaks=breaks, xlab="", ylab="")
+#     axis(3, at=1:ncol(expr), labels=samplenames(object),tick=FALSE)
+#     axis(4, at=1:nrow(expr), labels=genenames(object), tick=FALSE, las=1)
+# })
+
+
+
+
+}
+
