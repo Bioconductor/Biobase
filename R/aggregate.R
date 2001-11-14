d09ffe0731d593a051d0a32816667e1dda27fe5d
Robert Gentleman <rgentlem@fhcrc.org>
2001-11-14 14:19:42 +0000

    changed aggregate
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@185 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/aggregate.R
+++ R/aggregate.R
@@ -7,6 +7,8 @@
 ##initfun, if they are there they get aggregated with
 ##agfun
 
+require("methods")
+
 Aggregate <- function(x, env, agfun, initfun)
  {
    if( !is.environment(env) )
@@ -39,3 +41,25 @@ Aggregate <- function(x, env, agfun, initfun)
    }
    else stop("bad type for Aggregate")
  }
+
+setClass("aggregator", representation( aggenv = "environment", initfun =
+                                     "function", aggfun = "function"),
+         prototype = list(env = new.env(hash=TRUE), initfun =
+                                     function(name, val) 1,
+         aggfun =  function(name, current, val) current+1 ) )
+
+
+if( !isGeneric("aggenv") )
+    setGeneric("aggenv", function(object) standardGeneric("aggenv"))
+
+setMethod("aggenv", "aggregator", function(object) object@aggenv)
+
+if( !isGeneric("initfun") )
+    setGeneric("initfun", function(object) standardGeneric("object"))
+
+setMethod("initfun", "aggregator", function(object) object@initfun)
+
+if( !isGeneric("aggfun") )
+    setGeneric("aggfun", function(object) standardGeneric("aggfun"))
+
+setMethod("aggfun", "aggregator", function(object) object@aggfun)
