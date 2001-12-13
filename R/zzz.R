fa6b1373653e77d818c62b1c74ddc01f98fa615d
Vince Carey <stvjc@channing.harvard.edu>
2001-12-13 13:10:08 +0000

    dumppacktxt added
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@262 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,26 @@
+# temporarily store this utility here
+
+dumpPackTxt <- function (package)
+{
+# stolen from "library" to get descriptive
+# info out on stdout
+#
+    pkgpath <- .find.package(package)
+#    outFile <- tempfile("Rlibrary")
+#    outConn <- file(outFile, open = "w")
+    docFiles <- file.path(pkgpath, c("TITLE", "DESCRIPTION",
+        "INDEX"))
+    headers <- c("", "Description:\n\n", "Index:\n\n")
+    footers <- c("\n", "\n", "")
+    for (i in which(file.exists(docFiles))) {
+        writeLines(headers[i], sep = "")
+        writeLines(readLines(docFiles[i]) )
+        writeLines(footers[i], sep = "")
+    }
+#    close(outConn)
+#    file.show(outFile, delete.file = TRUE)
+}
+
 .First.lib <- function(libname, pkgname, where) {
     require(methods)
     where <- match(paste("package:", pkgname, sep=""), search())
