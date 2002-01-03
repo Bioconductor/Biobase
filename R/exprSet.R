7100907f96dd8fd4fc3c0205a02f460679a024e6
Vince Carey <stvjc@channing.harvard.edu>
2002-01-03 21:16:44 +0000

    added esApply
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@301 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/exprSet.R
+++ R/exprSet.R
@@ -269,3 +269,14 @@ setMethod("iter", signature(object="exprSet", covlab="character",
 
 with.exprSet <- function(data, expr, ...) eval(substitute(expr),
   data@phenoData@pData, enclos=parent.frame())
+
+
+esApply <- function( es, f ) {
+ # assumes f is of the form f(arg1,arg2) and
+ # arg2 is the pData of es
+ if (class(es) != "exprSet") stop("arg1 must be of class exprSet")
+ if (length(formals(f)) != 2) warning("f should be a function of two arguments...") 
+ apply( exprs(es), 1, f, es )
+ }
+
+
