f156b97c6987ce09bef9909aa16679d6d4758750
Robert Gentleman <rgentlem@fhcrc.org>
2002-02-17 18:20:51 +0000

    added a new test to aggregate and formatted the text
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@472 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/aggregate.R
+++ R/aggregate.R
@@ -10,64 +10,64 @@
 require("methods")
 
 Aggregate <- function(x, agg)
- {
-   if( !inherits(agg, "aggregator") )
-     stop("second argument must be an aggregator")
-
-   if(is.character(x)) {
-     for( i in 1:length(x) ) {
-       nm <- x[i]
-       if( !exists(nm, env=aggenv(agg), inherits=FALSE) )
-          assign(nm, env=aggenv(agg), initfun(agg)(nm, x))
-       else {
-          v1 <- get(nm, env=aggenv(agg))
-          assign(nm, aggfun(agg)(nm, v1), env=aggenv(agg))
-       }
-     }
-   }
-   else if(is.list(x)) {
-     nms <- names(x)
-     for( i in 1:length(x) ) {
-        nm <- nms[i]
-        if( !exists(nm, env=aggenv(agg), inherits=FALSE) )
-          assign(nm, env=aggenv(agg), initfun(agg)(nm, x[[i]]))
-        else {
-           v1 <- get(nm, env=aggenv(agg))
-           assign(nm, env=aggenv(agg), aggfun(agg)(nm, v1, x[[i]]))
+{
+    if( !inherits(agg, "aggregator") )
+        stop("second argument must be an aggregator")
+    if( is.null(x) || length(x) == 0 )
+        return()
+    if(is.character(x)) {
+        for( i in 1:length(x) ) {
+            nm <- x[i]
+            if( !exists(nm, env=aggenv(agg), inherits=FALSE) )
+                assign(nm, env=aggenv(agg), initfun(agg)(nm, x))
+            else {
+                v1 <- get(nm, env=aggenv(agg))
+                assign(nm, aggfun(agg)(nm, v1), env=aggenv(agg))
+            }
         }
-     }
-   }
-   else stop("bad type for Aggregate")
- }
+    }
+    else if(is.list(x)) {
+        nms <- names(x)
+        for( i in 1:length(x) ) {
+            nm <- nms[i]
+            if( !exists(nm, env=aggenv(agg), inherits=FALSE) )
+                assign(nm, env=aggenv(agg), initfun(agg)(nm, x[[i]]))
+            else {
+                v1 <- get(nm, env=aggenv(agg))
+                assign(nm, env=aggenv(agg), aggfun(agg)(nm, v1, x[[i]]))
+            }
+        }
+    }
+    else stop("bad type for Aggregate")
+}
 
 .initAgg <- function(where) {
-setClass("aggregator", representation( aggenv = "environment",
-                                      initfun = "function",
-                                      aggfun = "function"),
-         prototype = list(aggenv = new.env(hash=TRUE), initfun =
-                                     function(name, val) 1,
-         aggfun =  function(name, current, val) current+1 ) , where=where)
+    setClass("aggregator", representation( aggenv = "environment",
+                                          initfun = "function",
+                                          aggfun = "function"),
+             prototype = list(aggenv = new.env(hash=TRUE), initfun =
+             function(name, val) 1,
+             aggfun =  function(name, current, val) current+1 ) , where=where)
 
 
-if( !isGeneric("aggenv") )
-    setGeneric("aggenv", function(object) standardGeneric("aggenv"),
+    if( !isGeneric("aggenv") )
+        setGeneric("aggenv", function(object) standardGeneric("aggenv"),
     where=where)
 
-setMethod("aggenv", "aggregator", function(object) object@aggenv,
-          where=where)
+    setMethod("aggenv", "aggregator", function(object) object@aggenv,
+              where=where)
 
-if( !isGeneric("initfun") )
-    setGeneric("initfun", function(object) standardGeneric("initfun"),
-    where=where)
+    if( !isGeneric("initfun") )
+        setGeneric("initfun", function(object) standardGeneric("initfun"),
+                   where=where)
 
-setMethod("initfun", "aggregator", function(object) object@initfun,
+    setMethod("initfun", "aggregator", function(object) object@initfun,
           where=where )
 
-if( !isGeneric("aggfun") )
-    setGeneric("aggfun", function(object) standardGeneric("aggfun"),
-    where=where )
+    if( !isGeneric("aggfun") )
+        setGeneric("aggfun", function(object) standardGeneric("aggfun"),
+                   where=where )
 
-setMethod("aggfun", "aggregator", function(object) object@aggfun,
-          where=where)
-
+    setMethod("aggfun", "aggregator", function(object) object@aggfun,
+              where=where)
 }
