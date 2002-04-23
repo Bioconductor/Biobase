c8625088990b164c3e6f7a8e1b1f5a6b6e86b0f3
jhnzhang <unknown>
2002-04-23 20:29:02 +0000

    Function that gets Bioconductor packages was added. This function does not belong to Biobase but just resides in the scripts directory.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@1053 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/scripts/getBioC.R
@@ -0,0 +1,113 @@
+# This function gets and installs the required Bioconductor libraries.
+#
+# libName: a character string for the name of the library to be
+# installed. Valid names include "all" - all the released packages,
+# "affy" - packages "affy" plus exprs, "CDNA" - packages "CDNA" plus
+# exprs, and "exprs" - packages "Biobase", "annotate", "genefilter",
+# "geneploter", "edd", "Roc", and "tkWidgets".
+# destdir: a character string for the directory where the downloaded
+# packages will be stored.
+# isDevel: a boolean indicating whether the released (FALSE) and
+# developer (TRUE) version will be downloaded and installed.
+# verbose: a boolean indicating whether any error related to the
+# downloading process will be (TRUE) printed. Error messages will
+# still be returned but invisible is berbose is set to FALSE.
+#
+getBioC <- function (libName = "exprs", destdir = NULL, isDevel = FALSE,
+                     verbose = TRUE){
+
+    on.exit(options(show.error.messages = TRUE))
+
+    PLATFORM <- .Platform$OS.type
+    DESTDIR <- ifelse(is.null(destdir), getwd(), destdir)
+    messages <- NULL
+
+    packs <- getPackNames(libName)
+    print(packs)
+    for(i in packs){
+        sourceUrl <- getUrl(PLATFORM, i, isDevel)
+        fileName <- getFName(PLATFORM, DESTDIR, i)
+
+        # check the connection instead of downloading directly which
+        # will write files of 0 size in the directory even when the
+        # the connection is not there.
+        options(show.error.messages = FALSE)
+        tryMe <- try(url(sourceUrl, "r"))
+        options(show.error.messages = TRUE)
+
+        if(inherits(tryMe, "try-error")){
+            if(verbose)
+                print(paste("Get", i, "failed"))
+            else
+                messages <- paste(messages, paste("Get", i, "failed"),
+                                  sep = "\n")
+        }else{
+            download.file(sourceUrl, fileName, quiet = TRUE)
+            installPack(PLATFORM, fileName)
+        }
+    }
+    return(invisible(messages))
+}
+
+getPackNames <- function (libName){
+    AFFY <- "affy"
+    CDNA <- c("marrayInput", "marrayClasses", "marrayNorm",
+           "marrayPlots")
+    EXPRS <-c("Biobase", "annotate", "genefilter", "geneploter",
+              "edd", "Roc", "tkWidgets")
+    switch(libName,
+           "all" = return(c(EXPRS, AFFY, CDNA)),
+           "exprs" = return(EXPRS),
+           "affy" = return(EXPRS, AFFY),
+           "cdna" =,
+           "CDNA" = return(c(EXPRS, CDNA)),
+           stop("The library is not valid"))
+}
+
+getUrl <- function(platform, pack, isDevel = FALSE){
+    if(isDevel)
+        tempUrl <-
+            "http://www.bioconductor.org/packages/distrib//devel/"
+    else
+        tempUrl <-
+            "http://www.bioconductor.org/packages/distrib//release/"
+     switch(platform,
+            "unix" = return (paste(tempUrl, "Source/",
+            pack, "_", getVersion(), ".tar.gz", sep = "")),
+            "windows" = return(paste(tempUrl,"Win32/",
+            pack, "-snapshot.zip", sep = "")),
+            stop("The OS system is not supported"))
+}
+
+getFName <- function(platform, destdir, pack){
+     switch(platform,
+            "unix" = return (paste(destdir, .Platform$file.sep,
+            pack, "_", getVersion(), ".tar.gz", sep = "")),
+            "windows" = return(paste(destdir, .Platform$file.sep,
+            pack, "-snapshot.zip")),
+            stop("The OS system is not supported"))
+}
+
+getVersion <- function(){
+    return("1.0")
+}
+
+installPack <- function(platform, fileName){
+    if(platform == "unix"){
+        system(paste("R CMD INSTALL ", fileName, sep = ""))
+    }else{
+        if(platform == "windows"){
+            output <- system(paste("Rcmd INSTALL", fileName, sep = ""))
+        }else{
+            stop("The OS system is not supported")
+        }
+    }
+}
+
+
+
+
+
+
+
+
