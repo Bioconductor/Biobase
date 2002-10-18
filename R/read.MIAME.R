768842bd61074daf1d1637b567d9d97a116da79c
dfcimm1 <unknown>
2002-10-18 18:22:01 +0000

    RI: new function for reading basic MIAME info
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@2008 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/read.MIAME.R
@@ -0,0 +1,24 @@
+read.MIAME <- function(filename=NULL,widget=getOption("BioC")$Base$use.widgets,...){
+  
+  if(!is.null(filename)){
+    miame <- scan(filename,what="c",quiet=TRUE,...)
+    return(new("MIAME",name=miame[1],lab=miame[2],contact=miame[3],title=miame[4],abstract=miame[5],url=miame[6]))
+  }
+  else
+    if(widget){
+      require(tkWidgets)
+      tmp <- tkMIAME()
+      return(new("MIAME",
+                 name=tmp$ExperimentName,
+                 lab=tmp$LabName,
+                 contact=tmp$ContactInfo,
+                 title=tmp$ExperimentTitle,
+                 abstract=tmp$Description,
+                 url=tmp$URL))
+    }
+           
+    else{
+      return(new("MIAME"))
+    }
+}
+          
