e267097782d61af265ebd8b6e8f33c890302aa49
dfcimm5 <unknown>
2003-07-18 18:32:59 +0000

    createPackage now uses a proper directory tree for the package template
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@5145 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/createpackage.R
+++ R/createpackage.R
@@ -1,19 +1,21 @@
 createPackage <- function(pkgname, destinationDir, originDir, symbolValues,
                           force=FALSE, quiet=FALSE)
 {
-  if(any(!is.character(c(pkgname, destinationDir, originDir))))
-    stop(paste("Arguments pkgname, destinationDir, originDir must be character strings."))
+  ## check arguments 
+  for (a in c("pkgname", "destinationDir", "originDir"))
+    if (!is.character(get(a)) || length(get(a))!=1)
+      stop(paste("'", a, "' must be a character vector of length 1.", sep=""))
   
-  ## check whether destinationDir and originDir exist and are directories
-  fi <- file.info(c(destinationDir, originDir))
-  if(!all(fi$isdir))
-    stop(paste("destinationDir:", destinationDir, "and originDir:", originDir,
-               "must be directories."))
+  ## check whether destinationDir, originDir exist and are directories
+  for (a in c("destinationDir", "originDir"))
+    if(!file.info(get(a))$isdir)
+      stop(paste("'", a, "' must be a directory (", get(a), ")\n.", sep=""))
 
-  ## create the package directories
+  ## locate / remove / create destination directory
   pkgdir = file.path(destinationDir, pkgname)
   if (!quiet)
     cat("Creating package in", pkgdir, "\n")
+  
   if (file.exists(pkgdir)) {
     if (force) {
       unlink(pkgdir, recursive=TRUE)
@@ -27,34 +29,15 @@ createPackage <- function(pkgname, destinationDir, originDir, symbolValues,
     } else {
       stop(paste("Directory", pkgdir, "exists. Please use force=TRUE to remove it",
                  "or choose another destination directory."))
-    } ## if(force)else 
+    } ## if (force) else 
   }  ## if (file.exists)
-  
-  for (d in c("", "man", "src", "R", "data")) {
-    res = dir.create(file.path(pkgdir, d))
-    if(!res)
-      stop(paste("Failed to create directory", file.path(pkgdir, d)))
-  }
 
   ## predefined symbols
   symbolValues = append(symbolValues, list(TODAY=date(), PKGNAME=pkgname))
-  
-  ## DESCRIPTION
-  copySubstitute(file.path(originDir, "DESCRIPTION"),
-                 pkgdir, symbolValues, recursive=TRUE)
+
+  ## copy
+  copySubstitute(dir(originDir, full.names=TRUE), pkgdir, symbolValues, recursive=TRUE)
     
-  ## R files
-  for (fn in list.files(originDir, pattern="*\\.R$"))
-    copySubstitute(file.path(originDir, fn), file.path(pkgdir, "R"), symbolValues, recursive=TRUE)
-  
-  ## C files
-  for (fn in list.files(originDir, pattern="*\\.c$"))
-    copySubstitute(file.path(originDir, fn), file.path(pkgdir, "src"), symbolValues, recursive=TRUE)
-  
-  ## man files
-  for (fn in list.files(originDir, pattern="*\\.Rd$"))
-    copySubstitute(file.path(originDir, fn), file.path(pkgdir, "man"), symbolValues, recursive=TRUE)
-  
   return(list(pkgdir=pkgdir))
 }
 
