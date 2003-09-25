088d9ae312088566176cf99c3f1b67684eb9e703
Jeff Gentry <jgentry@jimmy.harvard.edu>
2003-09-25 17:05:00 +0000

    remove tkWidget dependency
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@5804 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.MIAME.R
+++ R/read.MIAME.R
@@ -1,12 +1,12 @@
 read.MIAME <- function(filename=NULL,widget=getOption("BioC")$Base$use.widgets,...){
-  
+
   if(!is.null(filename)){
     miame <- scan(filename,what="c",quiet=TRUE,sep="\n",...)
     return(new("MIAME",name=miame[1],lab=miame[2],contact=miame[3],title=miame[4],abstract=miame[5],url=miame[6]))
   }
   else
     if(widget){
-      require(tkWidgets)
+      require(tkWidgets) || stop("Requires tkWidgets")
       tmp <- tkMIAME()
       return(new("MIAME",
                  name=tmp$ExperimentName,
@@ -16,9 +16,9 @@ read.MIAME <- function(filename=NULL,widget=getOption("BioC")$Base$use.widgets,.
                  abstract=tmp$Description,
                  url=tmp$URL))
     }
-           
+
     else{
       return(new("MIAME"))
     }
 }
-          
+
