50229857c572000b1ec8057ff9b052db58b9d34b
Robert Gentleman <rgentlem@fhcrc.org>
2004-03-12 14:52:14 +0000

    updating docs and environment functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@7770 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/envir.R
+++ R/envir.R
@@ -1,21 +1,5 @@
-env2list <- function(envir, recurse=FALSE) {
-    if (!is.environment(envir))
-        stop("envir argument is not an environment")
-
-    ans <- list()
-    ## This is basically a lot like multiget() except there isn't
-    ## need for checking that what you're looking for exists as
-    ## you know it does
-    vals <- ls(envir)
-    for (i in seq(along=vals))
-        ans[[i]] <- get(vals[i], envir=envir, inherits=FALSE)
-    names(ans) <- vals
-
-    if ((recurse == TRUE)&&(!is.null(parent.env(envir))))
-        ans <- c(ans, list2env(parent.env(envir), recurse))
-
-    ans
-}
+##Copyright R. Gentleman, 2004, all rights reserved
+##Functions for lists -> in C for efficiency
 
 listLen <- function(list)
     .Call("listLen", list, PACKAGE="Biobase")
@@ -27,32 +11,9 @@ l2e <- function(vals, envir) {
     .Call("listToEnv", vals, envir, PACKAGE="Biobase")
 }
 
-##FIXME: not sure that recurse makes sense here
-list2env <- function(vals, envir, recurse=FALSE) {
-    if (!is.environment(envir))
-        stop("envir argument is not an environment")
-
-    if (!is.list(vals))
-        stop("vals argument is not a list")
-
-    len <- length(vals)
-    if (len == 0)
-        return(envir)
-
-    names <- names(vals)
-    if (length(names) != len)
-        stop("Not a properly named list")
-
-    ## !!! Does this work the way it is intended when recurse=TRUE?
-    for (i in 1:len)
-        assign(names[i], vals[[i]], envir=envir, inherits=recurse)
-
-    envir
-}
 
 copyEnv <- function(oldEnv, newEnv=new.env(hash=TRUE,
-                    parent=parent.env(oldEnv)), recurse=FALSE) {
-    oldVals <- env2list(oldEnv, recurse)
-    newEnv <- list2env(oldVals, newEnv)
-    return(newEnv)
+                    parent=parent.env(oldEnv)), all.names=FALSE) {
+    oldVals <- as.list(oldEnv, all.names)
+    l2e(oldVals, newEnv)
 }
