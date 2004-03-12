0688780f73858fb7864c6f60be300067b3684165
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-03-12 17:21:32 +0000

    change to using as.list in contents
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@7779 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/contents.R
+++ R/contents.R
@@ -1,7 +1,12 @@
   if( !isGeneric("contents") && !exists("contents", mode="function") )
-       setGeneric("contents", function(object)
+       setGeneric("contents", function(object, all.names)
                   standardGeneric("contents"))
 
   setMethod("contents", "environment",
-     function(object)
-         mget(ls(env=object), env=object, ifnotfound=NA))
+     function(object, all.names) {
+         if (missing(all.names))
+             all.names <- FALSE
+         as.list(object, all.names=all.names)
+     })
+
+
