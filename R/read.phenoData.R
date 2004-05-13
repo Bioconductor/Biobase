0948087026bbf60b607f40781ccb90a1d179ef5d
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-05-13 19:43:51 +0000

    rolling back out Jianhua's changes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@8633 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.phenoData.R
+++ R/read.phenoData.R
@@ -1,36 +1,30 @@
-read.phenoData <- function(filename = NULL, sampleNames = NULL,
-                           widget = getOption("BioC")$Base$use.widgets,...){
-
+read.phenoData <- function(filename=NULL,sampleNames=NULL,widget=getOption("BioC")$Base$use.widgets,...){
+  
+  if(!is.null(filename)){
+    pData <- read.table(filename,...)
+    if(!is.null(sampleNames)) row.names(pData) <- sampleNames
+    varLabels <- as.list(rep("read from file",ncol(pData)))
+    names(varLabels) <- names(pData)
+    return(new("phenoData",pData=pData,varLabels=varLabels))
+  }
+  else{
     if(widget){
-        require("tkWidgets") || stop("Package tkWidgets unavailable")
-        if(!is.null(filename)){
-            phenoD <- importPhenoData(fileName = filename,
-                               sampleNames = sampleNames, from = "file")
-        }else{
-            phenoD <- importPhenoData(sampleNames = sampleNames)
-        }
-        return(phenoD)
-    }else{
-        if(!is.null(filename)){
-            pData <- read.table(filename,...)
-            if(!is.null(sampleNames)) row.names(pData) <- sampleNames
-            varLabels <- as.list(rep("read from file",ncol(pData)))
-            names(varLabels) <- names(pData)
-            return(new("phenoData",pData=pData,varLabels=varLabels))
-        }else{
-            if(is.null(sampleNames)){
-                return(new("phenoData")) ##return a blank!
-            }else{
-                pdata <- data.frame(sample=1:length(sampleNames),
-                                    row.names=sampleNames)
-                return(new("phenoData",pData=pdata,
-                           varLabels=list(sample="arbitrary numbering")))
-            }
-        }
+      require(tkWidgets)
+      if(is.null(sampleNames)) stop("To use widget you must supply sampleNames.\n")
+      tmp <- tkphenoData(sampleNames)
+      pdata <- data.frame(tmp$pData)
+      varlabels <- as.list(tmp$varLabels[,1])
+      names(varlabels) <- rownames(tmp$varLabels)
+      return(new("phenoData",pData=pdata,varLabels=varlabels))
     }
+    else{
+      if(is.null(sampleNames)){
+        return(new("phenoData")) ##return a blank!
+      }
+      else{
+        pdata <- data.frame(sample=1:length(sampleNames),row.names=sampleNames)
+        return(new("phenoData",pData=pdata,varLabels=list(sample="arbitrary numbering")))
+      }
+    }
+  }
 }
-
-
-
-
-
