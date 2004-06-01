429e8c80a06beedd1a2eed9953da3f958abac711
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-06-01 17:49:12 +0000

    removing multiget in the devel  branch
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@8913 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/multi.R
+++ R/multi.R
@@ -5,37 +5,6 @@
 #FIXME: I think Luke's Dynamic variables should be used rather than
 #the on.exit kludge
 
-multiget <- function(x, pos=-1, envir=as.environment(pos), mode =
-                     "any",inherits = TRUE, iffail=NA)
-{
-    .Deprecated("mget", "base")
-    lenx <- length(x)
-    ans <- vector("list", length=lenx)
-    if( ! is.environment(envir) )
-        stop("envir argument is not an environment")
-    options(show.error.messages = FALSE)
-    on.exit(options(show.error.messages = TRUE))
-    for(i in 1:lenx)
-        if( is.list(x) )
-            ans[[i]] <- try(get(x[[i]],pos,envir, mode, inherits))
-        else
-            ans[[i]] <- try(get(x[i],pos,envir, mode, inherits))
-    options(show.error.messages = TRUE)
-    on.exit(NULL)
-
-    failfun <- function(x) {
-        cx <- class(x)
-        if( !is.null(cx) && cx == "try-error")
-            TRUE
-        else
-            FALSE
-    }
-    failed <- sapply(ans, failfun)
-    ans[failed] <- iffail
-
-    names(ans) <- x
-    ans
-}
 
 multiassign <- function (x, value, envir = parent.frame(), inherits =
                          FALSE)
