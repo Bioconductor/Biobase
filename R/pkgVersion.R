d9447bc2df35311fc46e4b10b64d2eb3d67bfe95
Robert Gentleman <rgentlem@fhcrc.org>
2004-07-07 20:56:08 +0000

    fixed a bug and bumped version no
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@9355 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pkgVersion.R
+++ R/pkgVersion.R
@@ -4,7 +4,7 @@ package.version <- function(pkg, lib.loc = NULL) {
     on.exit(options(warn=curWarn),add=TRUE)
     options(warn=-1)
 
-    desc <- package.description(pkg, lib.loc, "Version")
+    desc <- packageDescription(pkg, lib.loc, "Version")
     if (is.na(desc))
         stop(paste("Package",pkg,"does not exist"))
     desc
