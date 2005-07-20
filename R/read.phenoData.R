276e8bd138229560f3b46f3a8eea21a58339022e
Robert Gentleman <rgentlem@fhcrc.org>
2005-07-20 19:19:05 +0000

    added a new function for reading phenoData objects
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@13275 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.phenoData.R
+++ R/read.phenoData.R
@@ -1,3 +1,11 @@
+read.pD = function(filename = NULL) {
+  if( is.character(filename) || inherits(filename, "connection") )
+    df = read.table(filename)
+  else
+    stop("incorrect file")
+  df2pD(df)
+}
+
 read.phenoData <- function(filename = NULL, sampleNames = NULL,
                            widget = getOption("BioC")$Base$use.widgets,...){
 
@@ -11,13 +19,15 @@ read.phenoData <- function(filename = NULL, sampleNames = NULL,
         }
         return(phenoD)
     }else{
-        if(!is.null(filename)){
+        if(is.character(filename) || inherits(filename, "connection")){
             pData <- read.table(filename,...)
             if(!is.null(sampleNames)) row.names(pData) <- sampleNames
             varLabels <- as.list(rep("read from file",ncol(pData)))
             names(varLabels) <- names(pData)
             return(new("phenoData",pData=pData,varLabels=varLabels))
         }else{
+            if( !is.null(filename) )
+               stop("incorrect filename given")
             if(is.null(sampleNames)){
                 return(new("phenoData")) ##return a blank!
             }else{
