11b753962db43dec48a4ea61fb6806d9239de10b
Seth Falcon <sfalcon@fhcrc.org>
2006-01-23 18:57:24 +0000

    Only cat intro message on attach, not on load. This makes the package better for importing via NAMESPACE
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@15828 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -5,10 +5,12 @@
    # need contents to load at library attach - not at build time
    .initContents() # in environment.R
    .buildBiobaseOpts() # in environment.R
+}
+
+.onAttach <- function(libname, pkgname) {
    cat("\nWelcome to Bioconductor \n\n")
    cat("\tVignettes contain introductory material.\n")
    cat("\tTo view, simply type 'openVignette()' or start with 'help(Biobase)'. \n")
-   #cat("\t to see the available vignettes\n")
    cat("\tFor details on reading vignettes, see the openVignette help page.\n\n")
    ##set up menus -- windows only for now
    if( .Platform$OS.type == "windows" && .Platform$GUI == "Rgui" )
