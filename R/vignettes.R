c698b68aa3119903f23a47aed30feb3a1d9192b0
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-01-12 13:46:01 +0000

    nicer user interaction
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@21987 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/vignettes.R
+++ R/vignettes.R
@@ -1,66 +1,76 @@
 ## Functions to detect and open vignettes:
 ## uses tools.R
 
-getPkgVigs <- function(package=NULL) {
-    require("tools", quietly=TRUE) || stop("Requires the tools package")
-    pkgs <- .packages()
-    if( !is.null(package) ) {
-        if( !is.character(package) )
-            stop("`package' must be a character vector of package names")
-        rows <- match(package, pkgs)
-        if( all(is.na(rows)) )
-            stop("packages: ", paste(package,collapse=", "), " are not loaded")
-        if( any(is.na(rows)) )
-            warning("packages ", paste(package[is.na(rows)], collapse=", "), " are not loaded")
-        pkgs <- pkgs[rows[!is.na(rows)]]
+getPkgVigs = function(package=NULL) {
+  require("tools", quietly=TRUE) || stop("Requires the tools package")
+  pkgs <- .packages()
+  if( !is.null(package) ) {
+    if( !is.character(package) )
+      stop("`package' must be a character vector of package names")
+    rows <- match(package, pkgs)
+    if( all(is.na(rows)) )
+      stop("packages: ", paste(package,collapse=", "), " are not loaded")
+    if( any(is.na(rows)) )
+      warning("packages ", paste(package[is.na(rows)], collapse=", "), " are not loaded")
+    pkgs <- pkgs[rows[!is.na(rows)]]
+  }
+  vigrds = file.path(.find.package(pkgs), "Meta", "vignette.rds")
+  
+  ## construct data frame with: package, path, title
+  pkgVigs = vector(mode="list", length=length(vigrds))
+  for(j in seq(along=vigrds)) {
+    if (file.exists(vigrds[j])) {
+      v = .readRDS(vigrds[j])
+      f = v[, "PDF"]
+      f = ifelse(f=="",
+        as.character(NA),
+        file.path(dirname(dirname(vigrds[j])), "doc", f))
+      tit = v[, "Title"]
+      tit[tit==""] = as.character(NA)
+      rv= data.frame(package  = I(pkgs[j]),
+                     filename = I(f),
+                     title    = I(tit))
+      pkgVigs[[j]] = rv
     }
-    vigDirs <- file.path(.find.package(pkgs), "Meta", "vignette.rds")
-    vigs <- lapply(vigDirs,
-       function(x){
-          if (file.exists(x)) {
-             vigs <- .readRDS(x)
-             if (nrow(vigs) > 0) {
-                pdfname = vigs[, "PDF"]
-                tit     = vigs[, "Title"]
-                vigPaths = ifelse(pdfname=="", as.character(NA),
-                             file.path(dirname(x), "..", "doc", pdfname))
-                names(vigPaths) = ifelse(tit=="", "(no title)", tit)
-                vigPaths
-             } # else NULL
-          } # else NULL
-       }
-    )
-    unlist(vigs[sapply(vigs, function(x) !is.null(x) && (length(x) > 0))])
+  } ## for j
+  do.call("rbind", args=pkgVigs)
 }
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-openVignette <- function(package=NULL) {
-    vigFiles <- getPkgVigs(package)
-    if(is.null(vigFiles)) {
-      warning(paste(sep="", "No vignettes found",
-       ifelse(is.null(package), ".\n", sprintf(" for package%s %s.\n",
-          ifelse(length(package)==1, "", "s"), paste(package, collapse=", ")))))
-    } else {
-      nm <- paste("",names(vigFiles))  ##indent a little
-      
-      ##FIXME: why set names to NULL?
-      names(vigFiles) <- NULL
-      index <- menu(nm, title="Please select (by number) a vignette")
 
-      if (index > 0) {
-        vi = vigFiles[index]
-        if(!is.na(vi)) {
-          openPDF(vi)
-          cat("Opening", vi, "\n")
-          ## browseURL(paste("file://", vigFiles[index],sep=""))
-        } else {
-          stop("Sorry, this vignette seems to be corrupted on your system, cannot open it.\n",
-               "You might try to install the package.")
-        }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+openVignette = function(package=NULL) {
+  vig = getPkgVigs(package)
+  if(is.null(vig)) {
+    warning(paste(sep="", "No vignettes found",
+       ifelse(is.null(package), ".\n", sprintf(" for package%s %s.\n",
+              ifelse(length(package)==1, "", "s"), paste(package, collapse=", ")))))
+  } else {
+    hasnofile = is.na(vig$filename)
+    vig$title[hasnofile] = paste(vig$title[hasnofile], "[-]")
+    vig = vig[order(hasnofile, tolower(vig$package), tolower(vig$title)), ]
+    index = menu(paste(vig$package, "-", vig$tit),
+      title = paste("Please select a vignette:",
+        if(any(hasnofile))
+          "(entries marked by '[-]' have no PDF file)"
+        else
+          ""
+        ))
+
+    if (index>0) {
+      vif = vig$filename[index]
+      if(!is.na(vif)) {
+        openPDF(vif)
+        cat("Opening", vif, "\n")
+        ## browseURL(paste("file://", vig[index],sep=""))
+      } else {
+        stop("Sorry, no PDF file could be found for this vignette.\n",
+             "Please reinstall the package with built vignettes.")
       }
     }
+  }
 }
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# add vignetts or other elements to the menu bar of a window (2002 J. Zhang)
+# add vignettes or other elements to the menu bar of a window (2002 J. Zhang)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 addVigs2WinMenu <- function(pkgName) {
     if ((.Platform$OS.type == "windows") && (.Platform$GUI == "Rgui")
