fd44ebff051388c70da644f67b7de84c240e9113
Seth Falcon <sfalcon@fhcrc.org>
2007-03-20 22:43:32 +0000

    Use size arg for new.env in l2e and copyEnv
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@23341 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/environment.R
+++ R/environment.R
@@ -100,12 +100,14 @@ listLen <- function(list)
    .Call("listLen", list, PACKAGE="Biobase")
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 l2e <- function(vals, envir) {
-   if(missing(envir)) envir <- new.env(hash=TRUE)
+   if(missing(envir))
+     envir <- new.env(hash=TRUE, parent=emptyenv(), size=length(vals) * 1.20)
    .Call("listToEnv", vals, envir, PACKAGE="Biobase")
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 copyEnv <- function(oldEnv, newEnv=new.env(hash=TRUE,
-                    parent=parent.env(oldEnv)), all.names=FALSE) {
+                    parent=parent.env(oldEnv), size=length(oldEnv) * 1.20),
+                    all.names=FALSE) {
    oldVals <- as.list(oldEnv, all.names)
    l2e(oldVals, newEnv)
 }
