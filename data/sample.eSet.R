c1b57248a79c18c1421367ee237eb49e6921c0c2
Martin Morgan <mtmorgan@fhcrc.org>
2007-04-23 00:18:09 +0000

    Update S4 data instances
    Correct data/sample.eSet.R; update data/SW.rda
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@24107 bc3139a8-67e5-0310-9ffc-ced21a209358

--- data/sample.eSet.R
+++ data/sample.eSet.R
@@ -1,5 +1,5 @@
 deprecationWarning <- function() {
-    if (interactive() && "Biobase" %in% loadNamespace())
+    if (interactive() && "Biobase" %in% loadedNamespaces())
         .Deprecated(msg="This data set is deprecated. Use 'as(sample.eSet, \"MultiSet\")' to update")
 }
 deprecationWarning()
