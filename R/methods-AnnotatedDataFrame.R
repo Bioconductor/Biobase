f40b2199f48ec7e01f7279054fadd09ceca38665
Martin Morgan <mtmorgan@fhcrc.org>
2007-05-04 16:23:01 +0000

    Separate setMethod and function definition for annotatedDataFrameFrom
    This allows methods for new classes (e.g., BufferedMatrix) to reuse
    code without 'tricking' method dispatch. Function defintions remain
    not exported, to discourage the end user from accessing directly.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@24510 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-AnnotatedDataFrame.R
+++ R/methods-AnnotatedDataFrame.R
@@ -51,49 +51,56 @@ setMethod("updateObject", signature(object="AnnotatedDataFrame"),
               }
           })
 
+annotatedDataFrameFromMatrix <- function(object, byrow=FALSE, ...) {
+    ## contract: 'object' is matrix-like, with dim, rownames, colnames
+    ## methods. Returns AnnotatedDataFrame with appropriate dimensions.
+    dims <- dim(object)
+    if (is.null(dims) || all(dims==0))
+        annotatedDataFrameFrom(NULL, byrow=byrow, ...)
+    else {
+        n <- if (byrow) dims[1] else dims[2]
+        nms <-
+            if(byrow) rownames(object)
+            else colnames(object)
+        data <- data.frame(numeric(n), row.names=nms)[,FALSE]
+        dimLabels <-
+            if (byrow) c("featureNames", "featureColumns")
+            else c("sampleNames", "sampleColumns")
+        new("AnnotatedDataFrame", data=data, dimLabels=dimLabels)
+    }
+}
+
 setMethod("annotatedDataFrameFrom",
           signature(object="matrix"),
-          function(object, byrow=FALSE, ...) {
-              dims <- dim(object)
-              if (is.null(dims) || all(dims==0))
-                annotatedDataFrameFrom(NULL, byrow=byrow, ...)
-              else {
-                  n <- if (byrow) dims[1] else dims[2]
-                  nms <-
-                    if(byrow) rownames(object)
-                    else colnames(object)
-                    data <- data.frame(numeric(n), row.names=nms)[,FALSE]
-                  dimLabels <-
-                    if (byrow) c("featureNames", "featureColumns")
-                    else c("sampleNames", "sampleColumns")
-                  new("AnnotatedDataFrame", data=data, dimLabels=dimLabels)
-              }
-          })
+          annotatedDataFrameFromMatrix)
+
+annotatedDataFrameFromNull <- function(object, byrow=FALSE, ...) {
+    dimLabels <-
+        if (byrow) c("featureNames", "featureColumns")
+        else c("sampleNames", "sampleColumns")
+    new("AnnotatedDataFrame", data=data.frame(), dimLabels=dimLabels)
+}
 
 setMethod("annotatedDataFrameFrom",
           signature(object="NULL"),
-          function(object, byrow=FALSE, ...) {
-              dimLabels <-
-                if (byrow) c("featureNames", "featureColumns")
-                else c("sampleNames", "sampleColumns")
-              new("AnnotatedDataFrame", data=data.frame(), dimLabels=dimLabels)
-          })
+          annotatedDataFrameFromNull)
+
+annotatedDataFrameFromAssayData <- function(object, byrow=FALSE, ...) {
+    eltNames <-
+        if (is(object, "environment")) ls(object)
+        else names(object)
+    if (length(eltNames)==0)
+        annotatedDataFrameFrom(NULL, byrow=byrow, ...)
+    else
+        annotatedDataFrameFrom(object[[eltNames[1]]], byrow=byrow, ...)
+}
 
 setMethod("annotatedDataFrameFrom",
           signature(object="AssayData"),
-          function(object, byrow=FALSE, ...) {
-              eltNames <-
-                if (is(object, "environment")) ls(object)
-                else names(object)
-              if (length(eltNames)==0)
-                annotatedDataFrameFrom(NULL, byrow=byrow, ...)
-              else
-                annotatedDataFrameFrom(object[[eltNames[1]]], byrow=byrow, ...)
-          })
+          annotatedDataFrameFromAssayData)
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 setMethod("dim", "AnnotatedDataFrame", function( x ) {
-  d <- dim(x@data)
+  d <- dim(pData(x))
   names(d) <- dimLabels(x)
   d
 })
@@ -101,7 +108,7 @@ setMethod("dim", "AnnotatedDataFrame", function( x ) {
 setMethod("dimLabels", "AnnotatedDataFrame", function(object) {
     object@dimLabels
 })
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 setMethod("pData", "AnnotatedDataFrame", function(object) object@data)
 
 setReplaceMethod("pData", c("AnnotatedDataFrame", "data.frame"), function(object, value) {
