2668b291abe4de80145877b4725fec9298481dec
Oleg Sklyar <osklyar@ebi.ac.uk>
2007-08-08 21:20:17 +0000

    matchpt algorithm optimized for single argument calls, 10x speed up
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@26233 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/matchpt.c
+++ src/matchpt.c
@@ -5,7 +5,6 @@
 
 #include <Rinternals.h>
 #include <Rdefines.h>
-#include <float.h>
 
 #define INDEX(i, j, n) ((i) + (j)*(n))
 
@@ -14,45 +13,48 @@ SEXP matchpt(SEXP x, SEXP y) {
     int * dimx;
     int * dimy;
     double dist, mdist, tmp, * dres, *dx, *dy=NULL;
-    int i, j, k, index, nptx, npty, ncol, useY;
+    int i, j, k, index, nptx, npty, ncol, noY=1, startj;
     SEXP res, newDim;
 
     dimx = INTEGER(GET_DIM(x));
     nptx = dimx[0];
     npty = nptx;
+    dx = REAL(x);
+    dy = dx;
     if (y != R_NilValue) {
-        useY = 1;
+        dy = REAL(y);
         dimy = INTEGER(GET_DIM(y));
         npty = dimy[0];
+        noY = 0;
     }
-    else
-        useY = 0;
     ncol = dimx[1];
     PROTECT(res = allocVector(REALSXP, nptx * 2));
     dres = REAL(res);
-    dx = REAL(x);
-    if ( useY == 1 ) dy = REAL(y);
+    if (noY) 
+      for (i = 0; i < nptx * 2; i++) dres[i] = 0.0;
     for (i = 0; i < nptx; i++) {
+        if (noY && dres[INDEX(i, 0, nptx)] > 0 ) continue;
         index = i;
         mdist = R_PosInf;
-        for (j = 0; j < npty; j++) {
-            if (useY == 0 && i == j) continue;
+        startj = (noY) ? (i + 1):0;
+        for (j = startj; j < npty; j++) {
+            if (noY && (i == j || dres[INDEX(j, 0, npty)] > 0)) continue;
             dist = 0;
             for (k = 0; k < ncol; k++) {
-                if (useY == 1)
-                    tmp = dx[INDEX(i, k, nptx)] - dy[INDEX(j, k, npty)];
-                else
-                    tmp = dx[INDEX(i, k, nptx)] - dx[INDEX(j, k, nptx)];
+                tmp = dx[INDEX(i, k, nptx)] - dy[INDEX(j, k, npty)];
                 dist += tmp * tmp;
             }
-            dist = sqrt(dist);
             if (dist < mdist) {
                 index = j;
                 mdist = dist;
             }
         }
         dres[INDEX(i, 0, nptx)] = index + 1;
-        dres[INDEX(i, 1, nptx)] = mdist;
+        dres[INDEX(i, 1, nptx)] = sqrt(mdist);
+        if (noY) {
+          dres[INDEX(index, 0, nptx)] = i + 1;
+          dres[INDEX(index, 1, nptx)] = sqrt(mdist);
+        }
     }
     PROTECT(newDim = allocVector(INTSXP, 2));
     INTEGER(newDim)[0] = nptx;
