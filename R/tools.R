db434141b7e6d1c6dc6b924558ca060ec539c9a1
Martin Morgan <mtmorgan@fhcrc.org>
2008-03-26 13:46:59 +0000

    reverseSplit returns list() when input is list()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@30681 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tools.R
+++ R/tools.R
@@ -59,6 +59,9 @@ openPDF <- function(file, bg=TRUE) {
 }
 # ==========================================================================
 reverseSplit = function(inList) {
+   if (length(inList)==0) {
+       return(inList)
+   }
    lens = sapply(inList, length)
    nms = rep(names(inList), lens)
    vals = unlist(inList)
