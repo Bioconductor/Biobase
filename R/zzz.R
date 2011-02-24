325e89fe619a4669095ff1b83b266cfda09d92f4
Martin Morgan <mtmorgan@fhcrc.org>
2011-02-24 14:31:27 +0000

    modify splash screen to use browseVignettes()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biobase@53250 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,7 @@
     packageStartupMessage(
         paste("\nWelcome to Bioconductor\n",
                 "Vignettes contain introductory material. To view, type",
-                "'openVignette()'. To cite Bioconductor, see",
+                "'browseVignettes()'. To cite Bioconductor, see",
                 "'citation(\"Biobase\")' and for packages 'citation(pkgname)'.\n", sep="\n  "))
    addVigs2WinMenu("Biobase") 
 }
