6c1b4ff775d7fd7ac375b1a9548fe06aacdba2f1
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-09-08 11:20:29 -0400

    remove .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -7,17 +7,6 @@
     .buildBiobaseOpts() ## in environment.R
 }
 
-.onAttach <- function(libname, pkgname) {
-    msg0 <- "Vignettes contain introductory material; view with
-             'browseVignettes()'. To cite Bioconductor, see
-             'citation(\"Biobase\")', and for packages
-             'citation(\"pkgname\")'."
-    msg <- strwrap(paste(msg0, collapse=""), exdent=4, indent=4)
-    packageStartupMessage(paste(c("Welcome to Bioconductor\n", msg),
-                                collapse="\n"), "\n")
-    addVigs2WinMenu("Biobase") 
-}
-
 .onUnload <- function( libpath ) {
   library.dynam.unload( "Biobase", libpath )
 }
